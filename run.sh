90708bf404e212fe7212f02269f67b0481204f88
Ken Moini <ken@kenmoini.com>
2020-02-09 15:56:45 -0600

    see if this will fix home

--- run.sh
+++ run.sh
@@ -3,8 +3,9 @@
 if [ `id -u` -ge 10000 ]; then
     echo "fixing passwd file..."
     #cat /etc/passwd | sed -e "s/^coder:/builder:/" > /tmp/passwd
-    cat /etc/passwd | sed -e "s/^coder:/#oldcoder:/" > /tmp/passwd
-    echo "coder:x:`id -u`:`id -g`:,,,:/home/coder:/usr/bin/zsh" >> /tmp/passwd
+    #cat /etc/passwd | sed -e "s/^coder:/#oldcoder:/" > /tmp/passwd
+    #echo "coder:x:`id -u`:`id -g`:,,,:/home/coder:/usr/bin/zsh" >> /tmp/passwd
+    cat /etc/passwd | sed -e "s;^coder:x:1000:1000:;coder:x:`id -u`:`id -g`:;" > /tmp/passwd
     cat /tmp/passwd > /etc/passwd
     rm /tmp/passwd
     chown -R coder /home/coder
